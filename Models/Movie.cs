8fac9ee2aa87ac5a3930ba0969fb258575800abb
Maryam Mohamed Auf <maryammohamedauf@gmail.com>
2024-11-10 14:33:34 +0200

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace radzen.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+
+        public string CoverImage { get; set; }
+    }
+}
---
+++ Services/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using radzen.Models;
+using radzen.Pages;
+
+namespace radzen.Services;
+public class MovieService
+{
+    private readonly HttpClient _httpClient;
+
+    public MovieService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Movie>> GetMoviesAsync()
+    {
+        var movies = await _httpClient.GetFromJsonAsync<List<Movie>>("data/movies.json");
+        Console.WriteLine($"Movies loaded: {movies?.Count ?? 0}");
+
+        return movies ?? new List<Movie>();
+
+    }
+}
